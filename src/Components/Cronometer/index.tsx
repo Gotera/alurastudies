b62002362a1040f3b43a1e2685c1488ecfe081c0
OM <orlandomedeiros.junior@gmail.com>
2022-12-03 19:21:21 -0300

    6.06

--- src/Components/Cronometer/index.tsx
+++ src/Components/Cronometer/index.tsx
@@ -1,8 +1,10 @@
+import { timeToSeconds } from "../../common/utils/date";
 import Button from "../Button";
 import Clock from "./Clock";
 import style from './Cronometer.module.scss'
 
 export default function Cronometer() {
+    console.log('conversao: ', timeToSeconds('01:01:01'))
     return (
         <div className={ style.cronometro }>
             <p className={ style.titulo }> Escolha um Card e inicie o cronômetro </p>
--- src/Components/List/Item/index.tsx
+++ src/Components/List/Item/index.tsx
@@ -1,9 +1,38 @@
 import { ITask } from '../../types/tarefa'
-import style from '../List.module.scss'
+import style from './Item.module.scss'
 
-export default function Item({ tarefa, tempo, selected, completed, id }: ITask ) {
+interface Props extends ITask{
+    selectTask: (selectedTask: ITask) => void;
+}
+
+export default function Item(
+    { 
+        tarefa, 
+        tempo, 
+        selected, 
+        completed, 
+        id, 
+        selectTask 
+    }: Props ) {
     return (
-        <li className={style.item}>
+        <li 
+            className=
+            {
+                `
+                    ${style.item} 
+                    ${selected ? style.itemSelecionado : ''} 
+                `
+            }
+            onClick={() => selectTask(
+                {
+                tarefa, 
+                tempo, 
+                selected, 
+                completed, 
+                id, 
+                })
+            }
+        >
         <h3> { tarefa } </h3>
         <span> { tempo } </span>
     </li>
